abacd27297f87151d705f0af23b2007e35459680
Daniel Sundström <daniel@sykewarrior.com>
2012-02-19 02:01:22 +0100

    cleanup

--- DYPL/Assignment2/runner.rb
+++ DYPL/Assignment2/runner.rb
@@ -1,10 +1,7 @@
 require './code_generation'
 
 person_klazz = Model.generate('person.txt')
-
 puts Person.inspect
-
 p = Person.new
 p.name = "Kalle"
 p.age = 23
-                                     
