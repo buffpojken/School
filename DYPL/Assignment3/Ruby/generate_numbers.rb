e43b51ac776081dcb0aeb539b7e08c48f7df17e7
Daniel Sundström <daniel@sykewarrior.com>
2012-03-17 21:51:28 +0100

    prepared for handin

--- DYPL/Assignment3/Ruby/generate_numbers.rb
+++
@@ -1,7 +0,0 @@
-f = File.new('nums', 'w+')
-
-2000000.times do |i|
-	f.puts rand(1000000000000)
-end                                    
-
-f.close
--- DYPL/Assignment3/Ruby/search3.rb
+++ DYPL/Assignment3/Ruby/search3.rb
@@ -1,6 +1,6 @@
 require 'benchmark'
 
-require './test_indexer'
+require './indexer'
 require './utils'     
 
 @glob = {}                 
@@ -9,41 +9,40 @@ require './utils'
 @db = {}
 
 def map(num, o)
-	stack = ""     
-	num.each_char do |letter|
-		stack += letter    
-		if @db.key?(stack)           
-			@stack.push stack        
-			@res_stack.push @db[stack]
- 			query = num.gsub(num.dup.slice!(0..stack.length-1),"")
-			map(query, o)
-	  end
-	end                            
-	@glob[@stack.join("")] = Array::new(@res_stack) if @stack.join("").length == o.length
-	@stack = []
-	@res_stack = []
-	return
+  stack = ""     
+  num.each_char do |letter|
+    stack += letter    
+    if @db.key?(stack)           
+      @stack.push stack        
+      @res_stack.push @db[stack]
+      query = num.gsub(num.dup.slice!(0..stack.length-1),"")
+      map(query, o)
+    end
+  end                            
+  @glob[@stack.join("")] = Array::new(@res_stack) if @stack.join("").length == o.length
+  @stack = []
+  @res_stack = []
+  return
 end      
 
 def reduce(res)   
-	res.each_pair do |number, value_list|   
-		res[number] = cartprod(*value_list)
-	end             
-	res
+  res.each_pair do |number, value_list|   
+    res[number] = cartprod(*value_list)
+  end             
+  res
 end    
 
-		indexer
-		i = 0
-		File.open('nums', 'r').each_line do |n|     
-			n.gsub("-", "").strip!    
-			m = map(n.dup, n)   
-			@res = reduce(@glob) 
-			@res.each_pair do |phone, result|  
-				result.each_with_index do |r, i|       
-						puts phone + ": " + r.join(" ").downcase.strip
-				end
-			end                                       
-			@glob = {}    
-			i += 1
-		end		
-       
+indexer(ARGV[0])
+i = 0
+File.open(ARGV[1], 'r').each_line do |n|     
+  n = n.gsub("-", "").strip
+  m = map(n.dup, n)   
+  @res = reduce(@glob) 
+  @res.each_pair do |phone, result|  
+    result.each_with_index do |r, i|       
+      puts phone + ": " + r.join(" ").downcase.strip
+    end
+  end                                       
+  @glob = {}    
+  i += 1
+end		
