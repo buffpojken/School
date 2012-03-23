03ed9c6651f74abe0c22f73330081bcd1215dbf5
buffpojken <daniel@sykewarrior.com>
2012-03-24 00:43:39 +0100

    added python

---
+++ LINSA/Labb6/print2lines.py
@@ -0,0 +1,15 @@
+#! /usr/local/bin/python
+
+# Import sys for access to argv, import glob for wildcard
+# file path matching
+import sys,glob
+
+# For each sent path (regardless of no)
+for path in sys.argv[1:]:
+  # For all files matching the pattern *.txt
+  for filepath in glob.glob(path+"*.txt"):
+    # Get a filehandle with read
+    file = open(filepath, 'r')
+    # Run readline() twice
+    for _ in xrange(2): print file.readline().strip()
+
